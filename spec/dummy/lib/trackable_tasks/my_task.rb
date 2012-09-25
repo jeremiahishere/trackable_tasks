5f87c8f04d6bf63030abc18b432c6f20f4e7891a
Alfredo Uribe <alfredouribe@Roozbeh-Meghdadis-iMac-2.local>
2012-09-25 10:46:47 -0400

    grouping instead of linear presentation
    logarithmic function for height
    vertical whitespace is calculated from previous task displayed not by
    time elapsed from 'now'
    overlap correction done in document ready

--- spec/dummy/lib/trackable_tasks/my_task.rb
+++ spec/dummy/lib/trackable_tasks/my_task.rb
@@ -2,6 +2,7 @@ class MyTask < TrackableTasks::Base
   
   # test of the tracking and logging systems
   def run
+    sleep(5+rand(120))
     log("Error level log", :error)
     log("Notice level log", :notice)
     log("Debug level log", :debug)
